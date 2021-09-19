925b30dbeed461b6b06d7f98bd043514d5967cf3
Mallikarjuna Desu <mallikarjunasai995@gmail.com>
2021-09-19 17:42:13 +0530

    function file changed

--- Functions.py
+++ Functions.py
@@ -1,14 +1,14 @@
 def auto_tune():
   return "singers will mostly do autotune when in pubic stages"
--------------------------------------------
+'''-------------------------------------------'''
 #parameters in functions
 def square(num):
   return num*num
-squre(4)
---------------------------------------------
+square(4)
+'''-------------------------------------------'''
 def yell(word):
     return f"{word.upper()}!"
- ---------------------------- ----------------
+'''-------------------------------------------'''
 #default parameters
 def speak(animal="dog"):
     noises = {"dog": "woof", "pig": "oink", "duck": "quack", "cat": "meow"}
@@ -16,7 +16,7 @@ def speak(animal="dog"):
     if noise:
         return noise
     return "?"
---------------------------------------------
+'''-------------------------------------------'''
   #keywordArguments
   #diff b/n keyword args and default paramters
   
