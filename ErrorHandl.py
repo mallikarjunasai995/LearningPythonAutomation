dd24720fbf08f69f4f6021dfbc900fa402bfe256
Mallikarjuna Desu <mallikarjunasai995@gmail.com>
2021-10-27 23:47:45 +0530

    added sepeate file for errorhandling

---
+++ ErrorHandl.py
@@ -0,0 +1,26 @@
+print("hi")
+def colorize(text, color):
+    if type(text) is not str:
+        #comment-firstdebugerrorhandling,valueerror
+        raise TypeError("text must be instance of str")
+    print(f"Printed {text} in {color}")
+colorize("hello","red")
+#try,except,finally
+d = {"1":"am first"}
+def get(d,key):
+    try:
+        return d[key]
+    except KeyError:
+        return None
+    
+
+#get(d,"asdf")
+
+def divide(a,b):
+    try: 
+        print(f"{a/b}")
+    except (TypeError,ValueError,ZeroDivisionError) as err:
+        print(err)
+    finally:
+        print("Obviously I came to this point")
+divide(1,0)
