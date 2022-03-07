43c30cf5cfbafb74b7c6621c8066f617d6ad4308
mallikarjunasai995 <mallikarjunasai995@gmail.com>
2022-03-07 08:12:27 +0530

    unittests

---
+++ TestingWithPython.py
@@ -0,0 +1,38 @@
+#TDD: RED, GREEN , REFRACTOR
+#write that tests fails - RED
+#write minimal amount of code that the test pass - GREEN
+
+#assertion
+
+# def eat_junk(food):
+#     assert food in ['pizza','burger'], "must be a junk food"
+#     return f"nom nonm nom am eating {food}"
+# food = input('enter a food please')
+# print(eat_junk(food))
+
+
+#doctestsexercisers:
+#python -m doctest -v filename.py
+
+def add(x,y):
+    """
+    >>> add(100,200) 
+    300
+    """
+    return x + y
+
+# def doubles(values):
+#     """double the values in a list
+#     >>>double([1,2,3,4])
+#     [2,4,6,8]
+#     >>>double([])
+#     []
+#     """
+#------------------------------------------------------------
+#------------------------------------------------------------
+#unittests : testing with small individual components
+
+
+
+
+    
